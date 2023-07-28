4e2990808c3821f05d46a13a18d81a8ad9893e86
ThetNaingOo2004 <thenaing8814@gmail.com>
2023-07-28 21:59:12 +0630

    I was finish Frontend part

---
+++ javascrip/pass-show-hide.js
@@ -0,0 +1,13 @@
+const pswrdField = document.querySelector(".form .field input[type='password']");
+toogleBtn = document.querySelector(".form .field i");
+
+toogleBtn.onclick = ()=>{
+    if(pswrdField.type == "password"){
+        pswrdField.type = "text";
+        toogleBtn.classList.add("active");
+    }
+    else{
+        pswrdField.type = "password";
+        toogleBtn.classList.remove("active");
+    }
+}
